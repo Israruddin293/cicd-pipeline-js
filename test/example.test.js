c59a429eb37a9cb64943592194df7d45c19eee22
Israr ud din <uisrar293@gmail.com>
2024-09-11 16:30:04 +0500

    cicd pipeline nodejs

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+// test/example.test.js
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', function() {
+  it('should pass', function() {
+    expect(true).to.be.true;
+  });
+});
